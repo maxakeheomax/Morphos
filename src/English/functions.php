380896db86590137e6c06bd84afe96459c338673
Maxim Boldyrev <maxakeheomax@mail.ru>
2022-01-22 16:21:40 +0300

    pluralize in english

--- src/English/functions.php
+++ src/English/functions.php
@@ -6,7 +6,7 @@ namespace morphos\English;
  * @param string $word
  * @return string
  */
-function pluralize($count, $word)
-{
-    return $count.' '.NounPluralization::pluralize($word, $count);
-}
+// function pluralize($count, $word)
+// {
+//     return $count.' '.NounPluralization::pluralize($word, $count);
+// }
